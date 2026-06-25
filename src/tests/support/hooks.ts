0157e7666aa296315bd7151efacd2c7e444c552a
Manish <k.manish72707@gmail.com>
2026-06-25 18:22:53 +0530

    new commit

--- src/tests/support/hooks.ts
+++ src/tests/support/hooks.ts
@@ -6,7 +6,7 @@ import { PageManager } from '../locators/POManager';
 setDefaultTimeout(30000);
 
 Before(async function (this: CustomWorld) {
-  this.browser = await chromium.launch({ headless: false });
+  this.browser = await chromium.launch({ headless: true });
   this.context = await this.browser.newContext();
   this.page = await this.context.newPage();
   this.pageLocator = new PageManager(this.page);
